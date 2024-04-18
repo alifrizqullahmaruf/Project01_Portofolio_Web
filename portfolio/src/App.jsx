539b87356fe9ef701e91ef0f4054c7f2076a5734
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-04-18 22:12:52 +0700

    Add some navbar for website react

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,11 @@
 // import React from 'react';
 
-import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
     </div>
   );
 }
--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,22 +1,33 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
+
 export default function Navbar(){
     return(
-        <>
-            <nav className="bg-gray-100">
-                <div className="px-4 mx-auto max-w-7xl ">
-                    <div className="flex items-center justify-between h-16">
-                        <div className="flex items-center">
-                            <a href="#" className="text-lg font-semibold text-black">Logo</a>
-                        </div>
-                        <div className="hidden sm:block">
-                            <div className="flex space-x-4">
-                                <a href="#" className="px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="#" className="px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:bg-gray-700 hover:text-white">Work</a>
-                                <a href="#" className="px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:bg-gray-700 hover:text-white">Contact</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </>
+
+        <div className="flex items-center justify-between h-24 px-4 text-white max-w-[1240px] mx-auto">
+            <h1 className="w-full text-3xl font-bold text-[rgb(0,223,154)]">REACT</h1>
+            <ul className="flex hidden">
+                <li className="p-4">Home</li>
+                <li className="p-4">Company</li>
+                <li className="p-4">Resaource</li>
+                <li className="p-4">About</li>
+                <li className="p-4">Contact</li>
+            </ul>
+            <AiOutlineMenu size={20} />
+            <div className="fixed top-0 left-0 border-r h--full w-[60%] border-r-gray-900 bg-[#000300]">
+            <h1 className="w-full text-3xl font-bold text-[rgb(0,223,154)] p-4">REACT</h1>
+                <ul className="p-4 uppercase">
+                    <li className="p-4 border-b border-b-gray-600">Home</li>
+                    <li className="p-4 border-b border-b-gray-600">Company</li>
+                    <li className="p-4 border-b border-b-gray-600">Resaource</li>
+                    <li className="p-4 border-b border-b-gray-600">About</li>
+                    <li className="p-4">Contact</li>
+                </ul>
+            </div>
+        </div>
     )
 }
+
+//? Noted
+// npm install react-icons --save
+// Kode diatas digunnakan untuk mengintall icon secar otomatis pada react link : https://www.npmjs.com/package/react-icons 
